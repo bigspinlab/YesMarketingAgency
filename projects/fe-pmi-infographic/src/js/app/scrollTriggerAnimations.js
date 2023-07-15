e78bdf0fecf6284f3107d7692fced4bbb58ba856
vitor-martinss <vitormartinsweb@gmail.com>
2023-07-15 14:28:55 +0100

    feat(pmi-infographic): wip fade animations

---
+++ projects/fe-pmi-infographic/src/js/app/scrollTriggerAnimations.js
@@ -0,0 +1,48 @@
+gsap.registerPlugin(ScrollTrigger);
+
+function gsapFadeAnimation(animationType, initialY, initialX) {
+  const animatedContainers = gsap.utils.toArray(`[data-gsap="${animationType}"]`)
+
+  animatedContainers.forEach(
+    (animation) => {
+      let delayedAnimation = 0;
+      if (animation.hasAttribute("data-gsap-delay")) {
+        delayedAnimation =
+          Number(animation.getAttribute("data-gsap-delay")) / 1000;
+      }
+
+      const animationFade = gsap.fromTo(
+        animation, {
+          autoAlpha: 0,
+          y: initialY,
+          x: initialX
+        }, {
+          duration: 1,
+          autoAlpha: 1,
+          y: 0,
+          x: 0,
+          delay: delayedAnimation
+        }
+      );
+      ScrollTrigger.create({
+        trigger: animation,
+        animation: animationFade,
+        start: "top 90%",
+        end: "top center",
+        //markers: true,
+        toggleActions: "play none none none",
+        //once: true,
+        // onEnter: self => {
+        //   // If it's scrolled past, set the state
+        //   // If it's scrolled to, play it
+        //   self.progress === 1 ? animationFade.progress(1) : animationFade.play()
+        // }
+      });
+    }
+  );
+}
+
+gsapFadeAnimation('fade-up', 100, 0);
+gsapFadeAnimation('fade-in', 0, 0);
+gsapFadeAnimation('fade-from-left', 0, -100);
+gsapFadeAnimation('fade-from-right', 0, 100);
