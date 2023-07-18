594094f6929311a196cae4d31ea3696c2d0dad21
vitor-martinss <vitormartinsweb@gmail.com>
2023-07-18 18:40:14 +0100

    fix(pmi-infographic): double scroll

--- projects/fe-pmi-infographic/src/js/app/scrollTriggerAnimations.js
+++ projects/fe-pmi-infographic/src/js/app/scrollTriggerAnimations.js
@@ -45,4 +45,11 @@ function initAnimations() {
   gsapFadeAnimation('fade-from-right', 0, 100);
 }
 
-initAnimations();
+function callAfterResize(func, delay) {
+  let dc = gsap.delayedCall(delay || 0.2, func).pause(),
+    handler = () => dc.restart(true);
+  window.addEventListener("resize", handler);
+  return handler; // in case you want to window.removeEventListener() later
+}
+
+callAfterResize(initAnimations());
