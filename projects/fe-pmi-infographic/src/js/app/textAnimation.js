845298f8d3d66f1ec0511347265cba2eabae6882
vitor-martinss <vitormartinsweb@gmail.com>
2023-07-20 20:58:21 +0100

    fix(pmi-infographic): bug on text animations

--- projects/fe-pmi-infographic/src/js/app/textAnimation.js
+++ projects/fe-pmi-infographic/src/js/app/textAnimation.js
@@ -1,25 +1,27 @@
-gsap.registerPlugin(ScrollTrigger);
+const textAnimations = () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-const textContainers = gsap.utils.toArray(".js-gsap-text");
-const pinnedSection = document.querySelector(".js-gsap-pinned");
+  const textContainers = gsap.utils.toArray(".js-gsap-text");
+  const pinnedSection = document.querySelector(".js-gsap-pinned");
 
-const animationTimeline = gsap.timeline({
-  scrollTrigger: {
-    trigger: pinnedSection,
-    start: "top top",
-    end: "bottom top",
-    pin: true,
-    scrub: true,
-  },
-})
-
-textContainers.forEach((text) => {
-  animationTimeline.fromTo(text, {
-    opacity: 0,
-  }, {
-    opacity: 1,
-    duration: 1,
-    yoyo: true,
-    repeat: 1,
+  const animationTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: pinnedSection,
+      pin: true,
+      scrub: true,
+    },
   })
-});
+
+  textContainers.forEach((text) => {
+    animationTimeline.fromTo(text, {
+      opacity: 0,
+    }, {
+      opacity: 1,
+      duration: 1,
+      yoyo: true,
+      repeat: 1,
+    })
+  });
+}
+
+textAnimations();
