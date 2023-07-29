dba026534135df2cbd6f3135274bd4d4aaf02e67
vitor-martinss <vitormartinsweb@gmail.com>
2023-07-29 20:25:47 +0100

    feat(boilerplate): deleted boilerplate

--- projects/fe-boilerplate/config/clean.js
+++
@@ -1,9 +0,0 @@
-import { config } from './_config.js';
-import { deleteAsync } from 'del';
-
-const clean = async done => {
-  await deleteAsync(`${config.views.index.dest}*`);
-  done();
-};
-
-export default clean;
